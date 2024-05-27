4f694610374ff5dc42ce7ee4b413cc9b07a7f3d6
Chiranjeev Vyas <75154547+thechiranjeevvyas@users.noreply.github.com>
2024-05-28 02:49:35 +0530

    slight changes

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -5,9 +5,9 @@ AOS.init();
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
-    title: "Software Development Intern",
+    title: "Graphic Designer and Social Media Marketer",
     cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
+    place: "Raema Star Solar",
     time: "(May, 2020 - present)",
     desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
   },
